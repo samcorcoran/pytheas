2af847e3bfaafd99872cea5541185497f2e6b20a
Sam Corcoran <samuel.corcoran@gmail.com>
2020-01-25 14:26:12 +0000

    Added triangle test

--- imageviewer.py
+++ imageviewer.py
@@ -1,11 +1,21 @@
 import pyglet
+import pyglet.gl
 
-window = pyglet.window.Window()
+width = 1000
+height = 1000
+
+window = pyglet.window.Window(960, 640)
 image = pyglet.resource.image('resources/pomeranian.jpg')
+
+def centre_image(image):
+    """Sets an image's anchor point to its center"""
+    image.anchor_x = image.width // 2
+    image.anchor_y = image.height // 2
 
 @window.event
 def on_draw():
     window.clear()
     image.blit(0, 0)
+    centre_image(image)
 
 pyglet.app.run()
