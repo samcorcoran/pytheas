d953ea2951af6e709e39e0e6dcc079b4d33c5ee0
Sam Corcoran <samuel.corcoran@gmail.com>
2020-12-22 20:33:16 +0000

    Colouring mode selection made more flexible
    Non-functional until functions are moved to cell_colouring.py

--- worldview.py
+++ worldview.py
@@ -31,14 +31,18 @@ class Window(pyglet.window.Window):
     indexed_domain = None
     indexed_vertex_list = None
     colouring_mode = 0
+    numbered_options = None
 
     def __init__(self, width, height, title=''):
         super(Window, self).__init__(width, height, title)
         glClearColor(0, 0, 0, 1)
         glEnable(GL_DEPTH_TEST)
+        self.set_up_options_ui()
         self.construct_world_for_drawing()
         self.set_colouring_mode(1)
 
+    def set_up_options_ui(self):
+        self.set_numbered_options()
     def construct_world_for_drawing(self):
         num_nodes = 0
         verts = list()
@@ -116,6 +120,18 @@ class Window(pyglet.window.Window):
             self.set_colouring_mode(3)
         if symbol == key._4:
             self.set_colouring_mode(4)
+        if symbol == key._5:
+            self.set_colouring_mode(5)
+        if symbol == key._6:
+            self.set_colouring_mode(6)
+        if symbol == key._7:
+            self.set_colouring_mode(7)
+        if symbol == key._8:
+            self.set_colouring_mode(8)
+        if symbol == key._9:
+            self.set_colouring_mode(9)
+        if symbol == key._0:
+            self.set_colouring_mode(0)
 
     def set_colouring_mode(self, new_render_mode):
         if self.colouring_mode == new_render_mode:
@@ -143,6 +159,8 @@ class Window(pyglet.window.Window):
         if recolour_ocean:
             everett_importer.update_ocean_cells_with_ocean_colours(self.indexed_vertex_list)
 
+        print("%s%d%s %s" % ("Mode ", new_render_mode, ".", self.numbered_options[new_render_mode][0]))
+        self.numbered_options[new_render_mode][1](self.indexed_vertex_list)
 
     def on_mouse_scroll(self, x, y, scroll_x, scroll_y):
         global current_scroll_level
@@ -152,6 +170,18 @@ class Window(pyglet.window.Window):
         current_scroll_level = min(max(current_scroll_level + scroll_y, 1), scroll_levels)
         self.on_resize(window_width, window_height)
 
+    def set_numbered_options(self):
+        self.numbered_options = list(range(10))
+        self.numbered_options[1] = ['Land colouring', cell_colouring.update_land_cells_with_flat_colour]
+        self.numbered_options[2] = ['Altitude colouring', cell_colouring.update_land_cells_with_altitude_colours]
+        self.numbered_options[3] = ['Whittaker colouring', cell_colouring.update_land_cells_with_whittaker_colours]
+        self.numbered_options[4] = ['Temperature colouring', cell_colouring.update_cells_with_temperature_colours]
+        self.numbered_options[5] = ['Toggle 2D/3D', self.toggle_2d_3d_mode]
+        self.numbered_options[6] = ['', print("Option 6 not set.")]
+        self.numbered_options[7] = ['', print("Option 7 not set.")]
+        self.numbered_options[8] = ['', print("Option 8 not set.")]
+        self.numbered_options[9] = ['', print("Option 9 not set.")]
+        self.numbered_options[0] = ['Reset ocean blues', cell_colouring.update_ocean_cells_with_ocean_colours]
 
 def main():
     global keys
