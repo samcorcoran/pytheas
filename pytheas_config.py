9684346184ee8c04089c9a673658406b1ef3d2ca
Sam Corcoran <samuel.corcoran@gmail.com>
2021-01-17 00:32:23 +0000

    Cell boundaries off by default

--- pytheas_config.py
+++ pytheas_config.py
@@ -4,6 +4,6 @@ number_of_cells = 10000
 
 # Render path options
 path_line_width = 2
-render_cell_boundaries = True
+render_cell_boundaries = False
 render_river_paths = True
 render_dummy_paths = False
